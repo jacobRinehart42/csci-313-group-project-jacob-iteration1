8ef1ed601d35350d62a5ce673f46376ed8dbfba9
jacobRinehart42 <jacobofmirkwood@yahoo.com>
2021-04-05 11:57:27 -0500

    1 file modified, 1 added

---
+++ src/app/account.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class AccountService {
+
+  accounts = [
+    {
+      username: "JimmyJohns64",
+      password: "johnlennin",
+      publicName: "Jimmy",
+      email: "macarthy@yahoo.com",
+      phoneNumber: "256-658-5314",
+      aboutYou: "Im a pizzaria owner"
+    },
+    {
+      username: "JackAnderson",
+      password: "jackRocx",
+      publicName: "JackAnderson",
+      email: "jackAnderson@gmail.com",
+      phoneNumber: "588-698-2547",
+      aboutYou: "He loves collecting niche items to expand his collection of souvenirs and knickknacks"
+    },
+    {
+      username: "DwightSchrute",
+      password: "bearsBeatsBattleStarGalactica",
+      publicName: "nightHawlk",
+      email: "weirdFargmer@bing.com",
+      phoneNumber: "584-698-2654",
+      aboutYou: "He would like to downsize his collection, and make sure that he gets not only what the item is worth but also ensure that the items end up with someone who appreciates them"
+    },
+    {
+      username: "JohnnyGuitar",
+      password: "electricSkado",
+      publicName: "Johnny Guitar",
+      email: "johnnyMcgee@alabama.com",
+      phoneNumber: "658-624-2547",
+      aboutYou: "He has inherited hundreds of collectable Hummel figurines and Venton glassware from his late mothers' estate. He would like to liquidate these materials and has found difficulty in finding the right place to market his items to interested buyers. "
+    },
+  ]
+
+  constructor() { }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { CreateAccountComponent } from './create-account/create-account.component';
+import { AccountService } from './account.service';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
   declarations: [ AppComponent, HelloComponent, CreateAccountComponent ],
-  bootstrap:    [ AppComponent ]
+  bootstrap:    [ AppComponent ],
+  providers: [AccountService]
 })
 export class AppModule { }
