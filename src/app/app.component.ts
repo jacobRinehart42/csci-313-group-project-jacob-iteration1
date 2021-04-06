9ccdff8c782f8ffcecb0b408fc5291841e9ae46d
jacobRinehart42 <jacobofmirkwood@yahoo.com>
2021-04-06 16:34:42 -0500

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component, VERSION } from '@angular/core';
+import { Component, VERSION } from "@angular/core";
+import { AccountService } from "./account.service";
 
 @Component({
-  selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  selector: "my-app",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+export class AppComponent {
+  name = "Angular " + VERSION.major;
+
+  accounts = this.acctService.accounts;
+  userHasSignedIn: boolean = false;
+  signedInUserAccount = {
+    id: 0,
+    username: "null",
+    password: "null",
+    publicName: "null",
+    email: "null",
+    phoneNumber: "null",
+    aboutYou: "null"
+  };
+
+  onCreateAccountSuccessfull(username: string) {
+    this.userHasSignedIn = true;
+  }
+
+  constructor(private acctService: AccountService) {}
+
+  ngOnInit() {}
 }
